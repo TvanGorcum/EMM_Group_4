7e9ff1ee109ee4c208443aa1ff2450dcd4e3204a
TvanGorcum <tvangorcum@gmail.com>
2025-10-21 18:06:56 +0200

    update settings and notebook

--- regression.py
+++ regression.py
@@ -26,9 +26,9 @@ def collect_subgroup_models(
     attr_config,
     *,
     beam_width: int = 30,
-    max_depth: int = 4,
+    max_depth: int = 3,
     min_support: int = 70,
-    top_S: int = 50,
+    top_S: int = 100,
 ) -> List[Dict[str, Any]]:
     results = emm_beam_search(
         df,
